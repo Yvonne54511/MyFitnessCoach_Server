fffb73d7869f30cf20d92a1f0cbea8133bcd2658
yvonne42396 <yvonne42396@gmail.com>
2026-04-09 17:31:15 +0800

    DI註冊

--- MyFitnessCoach_Server/Program.cs
+++ MyFitnessCoach_Server/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
+using MyFitnessCoach_Server.Models.EfModels;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// Register DbContext
+builder.Services.AddDbContext<MyFitnessCoachDbContext>(options =>
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configure CORS
+builder.Services.AddCors(options => {
+	options.AddPolicy("MyFitnessCoach_Client", policy => {
+		policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+	});
+});
+
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,8 +35,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseCors("MyFitnessCoach_Client");
 app.UseAuthorization();
+app.UseStaticFiles(new StaticFileOptions
+{
+	FileProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "StaticFiles")),
+	RequestPath = "/StaticFiles"
+});
 
 app.MapControllers();
 
