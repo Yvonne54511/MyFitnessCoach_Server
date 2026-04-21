565457bfb1eae5bad178d65d3bc116dd3cc36ddb
yvonne54511 <yvonne42396@gmail.com>
2026-04-21 19:10:38 +0800

    修改bug

--- MyFitnessCoach_Server/Models/EfModels/UserPasswordHistory.cs
+++ MyFitnessCoach_Server/Models/EfModels/UserPasswordHistory.cs
@@ -1,11 +1,17 @@
 #nullable disable
+using System;
+
 namespace MyFitnessCoach_Server.Models.EfModels;
 
-public class UserPasswordHistory
+public partial class UserPasswordHistory
 {
     public int Id { get; set; }
+
     public int UserId { get; set; }
+
     public string HashedPassword { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public virtual User User { get; set; }
 }
