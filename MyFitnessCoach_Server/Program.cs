7b3d0d587a93478d44f9c7877f4cdb73c2e4d6b0
Eric Chu <eric55339944@gmail.com>
2026-04-29 09:56:03 +0800

    顧客圖片

--- MyFitnessCoach_Server/Program.cs
+++ MyFitnessCoach_Server/Program.cs
@@ -110,6 +110,12 @@ app.UseStaticFiles(new StaticFileOptions
         Path.Combine(builder.Environment.ContentRootPath, "images")),
     RequestPath = "/images"
 });
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(builder.Environment.ContentRootPath, "img")),
+    RequestPath = "/img"
+});
 
 app.MapControllers();
 
